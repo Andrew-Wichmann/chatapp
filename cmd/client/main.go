5062030896ffb5cf094c4f60360baace8067e749
Andrew Wichmann <andrewp.wichmann@gmail.com>
2024-06-22 16:53:48 -0400

    Feat: add a textarea and use it's value to send to the server

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/Andrew-Wichmann/chatapp/pkg/client"
+	"github.com/charmbracelet/bubbles/textarea"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
@@ -14,14 +15,25 @@ type errMsg struct{ error }
 type App struct {
 	err       error
 	message   string
+	ta        textarea.Model
 	rpcClient client.ChatAppClient
 }
 
-func (app App) Init() tea.Cmd {
-	closure := func() tea.Msg {
-		return checkServer(app)
+func newApp() App {
+	ta := textarea.New()
+
+	ta.Placeholder = "Start chatting"
+	ta.Prompt = "┃ "
+	ta.ShowLineNumbers = false
+	ta.Focus()
+
+	return App{
+		ta: ta,
 	}
-	return closure
+}
+
+func (app App) Init() tea.Cmd {
+	return textarea.Blink
 }
 
 func (app App) View() string {
@@ -31,27 +43,38 @@ func (app App) View() string {
 	if app.err != nil {
 		return fmt.Sprintf("something went wrong: %s", app.err)
 	}
-	return "Processing. Press CTRL+C to exit"
+	if app.ta.Placeholder != "" {
+		return fmt.Sprintf("%s\nPress CTRL+C to exit", app.ta.View())
+	}
+	return "Initializing"
 }
 
 func (app App) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var (
+		tiCmd tea.Cmd
+	)
+
+	app.ta, tiCmd = app.ta.Update(msg)
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.Type {
 		case tea.KeyCtrlC:
 			return app, tea.Quit
+		case tea.KeyEnter:
+			cmd := sendMessage(app)
+			app.ta.Reset()
+			return app, cmd
 		}
 	case errMsg:
 		app.err = msg
 	case serverResponse:
 		app.message = string(msg)
 	}
-	return app, nil
+	return app, tiCmd
 }
 
 func main() {
-	app := App{}
-	prog := tea.NewProgram(app)
+	prog := tea.NewProgram(newApp())
 
 	prog.Run()
 }
@@ -64,4 +87,14 @@ func checkServer(app App) tea.Msg {
 	}
 
 	return serverResponse(message)
+}
+
+func sendMessage(app App) tea.Cmd {
+	return func() tea.Msg {
+		message, err := app.rpcClient.SendMessageRPC(app.ta.Value())
+		if err != nil {
+			return errMsg{err}
+		}
+		return serverResponse(message)
+	}
 }
