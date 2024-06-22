afdfea24dec28e747f0ef152c3827c39560829d8
Andrew Wichmann <andrewp.wichmann@gmail.com>
2024-06-22 15:20:34 -0400

    feat: issue an HTTP request and update the view with the response

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,32 +1,69 @@
 package main
 
-import tea "github.com/charmbracelet/bubbletea"
+import (
+	"fmt"
+	"net/http"
+	"time"
 
+	tea "github.com/charmbracelet/bubbletea"
+)
 
-type App struct{}
+type statusMsg int
+
+type errMsg struct{ error }
+
+type App struct {
+	err    error
+	status int
+}
 
 func (app App) Init() tea.Cmd {
-    return nil
+	return checkServer
 }
 
 func (app App) View() string {
-    return "Press CTRL+C to exit"
+	if app.status != 0 {
+		return fmt.Sprintf("Done! Got: %d. Press CTRL+C to exit", app.status)
+	}
+	if app.err != nil {
+		return fmt.Sprintf("something went wrong: %s", app.err)
+	}
+	return "Processing. Press CTRL+C to exit"
 }
 
 func (app App) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-    switch msg := msg.(type){
-    case tea.KeyMsg:
-        switch msg.Type{
-        case tea.KeyCtrlC:
-            return app, tea.Quit
-        }
-    }
-    return app, nil
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyCtrlC:
+			return app, tea.Quit
+		}
+	case errMsg:
+		app.err = msg
+	case statusMsg:
+		app.status = int(msg)
+	}
+	return app, nil
 }
 
 func main() {
-    app := App{}
-    prog := tea.NewProgram(app)
+	app := App{}
+	prog := tea.NewProgram(app)
 
-    prog.Run()
+	prog.Run()
+}
+
+var url = "http://google.com"
+
+func checkServer() tea.Msg {
+	c := &http.Client{
+		Timeout: 10 * time.Second,
+	}
+	res, err := c.Get(url)
+	if err != nil {
+		return errMsg{err}
+	}
+	defer res.Body.Close() // nolint:errcheck
+
+	return statusMsg(res.StatusCode)
 }
